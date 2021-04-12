97de29d55aa9bbaf137b1cf1413eef12b16d7293
nathanieldevo <nathanielsenou@gmail.com>
2021-04-12 09:13:54 +0000

    class personnage

--- Personnage.php
+++ Personnage.php
@@ -1,139 +1,114 @@
-<?php
+<?php 
 class Personnage
 {
-  private $_id;
-  private $_nom;
-  private $_forcePerso;
-  private $_degats;
-  private $_niveau;
-  private $_experience;
-/**
- * Hydrate
- */
-  public function hydrate(array $donnees)
-  {
-      # code...
-      if (isset['id']) {
-          # code...
-          $this->setId($donnees['id'])
-      }
-      if (isset['nom']) {
-          # code...
-          $this->setNom($donnees['nom'])
-      }
-      if (isset['forcePerso']) {
-          # code...
-          $this->_forcePerso=$donnees['forcePerso'];
-      }
-      if (isset['degats']) {
-          # code...
-          $this->_degats=$donnees['degats'];
-      }
-      if (isset['niveau']) {
-          # code...
-          $this->_niveau=$donnees['niveau'];
-      }
-      if (isset['experience']) {
-          # code...
-          $this->_experience=$donnees['experience'];
-      }
-  }
-  // Liste des getters
-  
-  public function id()
-  {
-    return $this->_id;
-  }
-  
-  public function nom()
-  {
-    return $this->_nom;
-  }
-  
-  public function forcePerso()
-  {
-    return $this->_forcePerso;
-  }
-  
-  public function degats()
-  {
-    return $this->_degats;
-  }
-  
-  public function niveau()
-  {
-    return $this->_niveau;
-  }
-  
-  public function experience()
-  {
-    return $this->_experience;
-  }
-  
-  // Liste des setters
-  
-  public function setId($id)
-  {
-    // On convertit l'argument en nombre entier.
-    // Si c'en était déjà un, rien ne changera.
-    // Sinon, la conversion donnera le nombre 0 (à quelques exceptions près, mais rien d'important ici).
-    $id = (int) $id;
-    
-    // On vérifie ensuite si ce nombre est bien strictement positif.
-    if ($id > 0)
-    {
-      // Si c'est le cas, c'est tout bon, on assigne la valeur à l'attribut correspondant.
-      $this->_id = $id;
-    }
-  }
-  
-  public function setNom($nom)
-  {
-    // On vérifie qu'il s'agit bien d'une chaîne de caractères.
-    if (is_string($nom))
-    {
-      $this->_nom = $nom;
-    }
-  }
-  
-  public function setForcePerso($forcePerso)
-  {
-    $forcePerso = (int) $forcePerso;
-    
-    if ($forcePerso >= 1 && $forcePerso <= 100)
-    {
-      $this->_forcePerso = $forcePerso;
-    }
-  }
-  
-  public function setDegats($degats)
-  {
-    $degats = (int) $degats;
-    
-    if ($degats >= 0 && $degats <= 100)
-    {
-      $this->_degats = $degats;
-    }
-  }
-  
-  public function setNiveau($niveau)
-  {
-    $niveau = (int) $niveau;
-    
-    if ($niveau >= 1 && $niveau <= 100)
-    {
-      $this->_niveau = $niveau;
-    }
-  }
-  
-  public function setExperience($experience)
-  {
-    $experience = (int) $experience;
-    
-    if ($experience >= 1 && $experience <= 100)
-    {
-      $this->_experience = $experience;
-    }
-  }
+    private $id;
+    private $nom;
+    private $degats;
+    // constantes
+    const CEST_MOI=1;
+    const PERSONNAGE_TUE=2;
+    const PERSONNAGE_FRAPPE=3;
+
 }
+/**
+ * Constructeur
+ * 
+ */
+public function __construct(array $donnees) {
+    $this->hydrate($donnees);
+}
+/**
+ * Methode frapper
+ */
+public function frapper( Personnage $perso)
+{
+    # code...
+    if ($perso->id()==$this->id) {
+        # code...
+        return self::CEST_MOI;
+    }
+    // retourne si le personnage a ete frappé ou deja tué
+    return recevoirDegats();
+}
+/**
+ * Methode recevoir degats
+ */
+public function recevoirDegats()
+{
+    # code...
+    $this->degats += 5;
+    if ($this->degats>=100) {
+        # code...Le peronnage est alors tué
+        return self::PERSONNAGE_TUE;
+    }
+    return self::PERSONNAGE_FRAPPE;
+}
+
+
+public function id()
+{
+    # code...
+    return $this->id;
+}
+public function nom()
+{
+    # code...
+    return $this->nom;
+}
+
+public function degats()
+{
+    # code...
+    return $this->degats;
+}
+
+public function setId($id)
+{
+    # code...
+    $id=int($id);
+    if ($id>0) {
+        # code...
+        $this->id=$id;
+    }
+}
+
+
+public function setNom($nom)
+{
+    # code...
+    if (is_string($nom)) {
+        # code...
+        $this->nom=$nom;
+    }
+
+}
+public function setDegats($degats)
+{
+    # code...
+    $degat=int($degat);
+    if ($degat<=100 && $degat>=0) {
+        # code...
+        $this->degats = $degats
+    }
+}
+
+
+
+public function hydrate(array $donnees)
+{
+    # code...
+    foreach ($donnees as $key => $value) {
+        # code...
+        $method='set'. ucfirst($key);
+        if (method_exist($this,$method)) {
+            # code...
+            $this->$method($value);
+        }
+    }
+}
+
+
+
+
+
 ?>
